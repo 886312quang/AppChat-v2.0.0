56b240c47f76da2a02627a9d6105bd77681aa40c
Quang Phạm Minh <886312quangpham@gmail.com>
2021-06-02 00:04:33 +0700

    jwt=

--- client/src/components/Shared/error/Error404Page.js
+++ client/src/components/Shared/error/Error404Page.js
@@ -4,31 +4,31 @@ import { Link } from "react-router-dom";
 import { Button } from "antd";
 
 const Error404Page = () => {
-    return (
-        <ErrorWrapper>
-            <div className="exception">
-                <div className="imgBlock">
-                    <div
-                        className="imgEle"
-                        style={{
-                            backgroundImage: `url(/images/404.svg)`
-                        }}
-                    />
-                </div>
-                <div className="content">
-                    <h1>404</h1>
-                    <div className="desc">Xin lỗi, trang bạn đang truy cập không tồn tại</div>
-                    <div className="actions">
-                        <Link to="/">
-                            <Button type="primary">
-                                Quay lại trang chủ
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </ErrorWrapper>
-    );
+  return (
+    <ErrorWrapper>
+      <div className="exception">
+        <div className="imgBlock">
+          <div
+            className="imgEle"
+            style={{
+              backgroundImage: `url(/images/404.svg)`,
+            }}
+          />
+        </div>
+        <div className="content">
+          <h1>404</h1>
+          <div className="desc">
+            Xin lỗi, trang bạn đang truy cập không tồn tại
+          </div>
+          <div className="actions">
+            <Link to="/">
+              <Button type="primary">Quay lại trang chủ</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </ErrorWrapper>
+  );
 };
 
 export default Error404Page;
